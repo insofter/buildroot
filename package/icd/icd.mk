9de804e7ad69be491df130bb04445d555c9e5183
Tomasz Rozensztrauch <trozensztrauch@gmail.com>
2012-03-11 19:42:15 +0100

    Updated version of icd package

--- package/icd/icd.mk
+++ package/icd/icd.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ICD_VERSION = 1.0
+ICD_VERSION = 0.0-49-g0b0c139-dirty
 ICD_SOURCE = icd-$(ICD_VERSION).tar.bz2
 ICD_SITE = http://www.insofter.pl
 ICD_DEPENDENCIES = sqlite libroxml libcurl
