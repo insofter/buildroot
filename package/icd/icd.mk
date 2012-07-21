5975b7e8a95796699aa1847383365e422791d6ef
Tomasz Rozensztrauch <trozensztrauch@gmail.com>
2012-07-21 08:37:39 +0200

    Fixed icd version

--- package/icd/icd.mk
+++ package/icd/icd.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ICD_VERSION = v0.0-59-gdff24c3
+ICD_VERSION = 0.0-59-gdff24c3
 ICD_SOURCE = icd-$(ICD_VERSION).tar.bz2
 ICD_SITE = http://www.insofter.pl
 ICD_DEPENDENCIES = sqlite libroxml libcurl
