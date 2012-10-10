08c283f544c210a3c143d818c2d52a3133fcf5d3
Piotr Mika <piotr.mt.mika@gmail.com>
2012-10-10 17:18:20 +0200

    new db cfg (global variables && autostart)

--- fs/skeleton-icdtcp3/etc/profile.d/icd.sh
+++ fs/skeleton-icdtcp3/etc/profile.d/icd.sh
@@ -1,3 +1,10 @@
+export ICD_DATA_DB=/home/data/data.db
+export ICD_CONFIG_DB=/home/data/config.db
+export ICD_LIVE_DB=/tmp/live.db
+
 export ICD_TEMP_DIR=/tmp
 export ICD_SHARE_DIR=/usr/share/icd
 export ICD_DATA_DIR=/home/data
+
+
+
